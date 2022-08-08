b869ba6d72f136b5836750e807ed08e2d2c45cb3
246859 <2633565580@qq.com>
2022-08-08 16:26:12 +0800

    feat : init
    项目初始化，搭建基本框架与构建基础依赖

---
+++ Lite-Blog-Web/src/main/java/com/liteweb/LiteBlogWebApplication.java
@@ -0,0 +1,11 @@
+package com.liteweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteBlogWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiteBlogWebApplication.class);
+    }
+}
