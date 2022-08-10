aa22c59cea2b1aab1479ea784f1c9a7dbeb104e9
246859 <2633565580@qq.com>
2022-08-10 17:22:45 +0800

    refactor(web.controller): content change

--- Lite-Blog-Web/src/main/java/com/liteweb/controller/HelloController.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.liteweb.controller;
 
+import com.liteweb.dto.ResultResponse;
+import com.liteweb.utils.ResultResponseUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String Hello(){
-        return "hello wolrd!";
+    public ResultResponse<String> Hello(){
+        return ResultResponseUtils.success("hello,world");
     }
 }
