fd1c59fe22995a60318476e6b31cd713fa111022
246859 <2633565580@qq.com>
2022-08-10 15:54:53 +0800

    feature(web): add helloController
    新增的hellocontroller，测试后端接口是否通畅

---
+++ Lite-Blog-Web/src/main/java/com/liteweb/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.liteweb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello wolrd!";
+    }
+}
