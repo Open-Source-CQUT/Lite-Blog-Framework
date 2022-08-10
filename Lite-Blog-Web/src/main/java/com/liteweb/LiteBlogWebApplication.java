3f3ad3710a5bc176b1d3ab704f28dce443b8aca6
246859 <2633565580@qq.com>
2022-08-10 15:59:57 +0800

    feature(web): add mapperScan crossOrigin
    启动类增加跨域和mapper扫描注解

--- Lite-Blog-Web/src/main/java/com/liteweb/LiteBlogWebApplication.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/LiteBlogWebApplication.java
@@ -1,9 +1,13 @@
 package com.liteweb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
+@MapperScan("com.liteweb.dao")
 public class LiteBlogWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(LiteBlogWebApplication.class);
