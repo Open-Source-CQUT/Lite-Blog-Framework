7f95d32eae15b9fb5e71beefc65c51004d3a50ad
246859 <2633565580@qq.com>
2022-08-13 16:13:12 +0800

    feature(web): finished regisger

---
+++ Lite-Blog-Web/src/main/java/com/liteweb/dao/auth/AuthMapper.java
@@ -0,0 +1,12 @@
+package com.liteweb.dao.auth;
+
+import com.liteweb.entity.auth.User;
+
+import java.util.Optional;
+
+public interface AuthMapper {
+
+    Optional<User> getUser(String mail);
+
+    Boolean insertUser(User user);
+}
