4911a577e67abef37c0d16de0bd8173b75a6b58f
246859 <2633565580@qq.com>
2022-08-13 16:12:52 +0800

    feature(web): finished regisger

--- Lite-Blog-Web/src/main/java/com/liteweb/controller/auth/AuthController.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/controller/auth/AuthController.java
@@ -1,24 +1,42 @@
 package com.liteweb.controller.auth;
 
+import com.liteweb.convert.auth.UserConverter;
 import com.liteweb.dto.global.ResultResponse;
-import com.liteweb.entity.auth.User;
+import com.liteweb.service.auth.AuthService;
+import com.liteweb.validation.Groups.NormalGroups;
+import com.liteweb.vo.Auth.UserVo;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
 @RestController
+@Slf4j
 @RequestMapping("/auth")
+@Validated
 public class AuthController {
 
+    @Autowired
+    AuthService service;
+
+    @Autowired
+    UserConverter userConverter;
+
     @PostMapping("/register")
-    public ResultResponse<Boolean> register(@RequestBody User user){
+    public ResultResponse<Boolean> register(@Validated(NormalGroups.Crud.Insert.class) UserVo userVo) {
 
         //TODO 注册接口
-        return null;
+        return service.register(userConverter.voToNormalDto(userVo));
     }
 
     @GetMapping("/login")
-    public ResultResponse<Boolean> login(@RequestParam String mail,@RequestParam String password){
+    public ResultResponse<Boolean> login(@RequestParam @Email @NotBlank String mail, @RequestParam @NotBlank String password) {
 
         //TODO 登陆接口
-        return null;
+        return service.login(mail,password);
     }
 }
--- Lite-Blog-Web/src/main/java/com/liteweb/service/auth/iml/AuthServiceIml.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/service/auth/iml/AuthServiceIml.java
@@ -1,10 +1,23 @@
 package com.liteweb.service.auth.iml;
 
+import com.liteweb.convert.auth.UserConverter;
+import com.liteweb.dao.auth.AuthMapper;
+import com.liteweb.dto.auth.UserNormalDto;
 import com.liteweb.dto.global.ResultResponse;
 import com.liteweb.entity.auth.User;
 import com.liteweb.service.auth.AuthService;
+import com.liteweb.utils.tool.ResultResponseUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthServiceIml implements AuthService {
+
+    @Autowired
+    UserConverter userConverter;
+
+    @Autowired
+    AuthMapper authMapper;
 
     @Override
     public ResultResponse<Boolean> login(String mail, String password) {
@@ -12,7 +25,14 @@ public class AuthServiceIml implements AuthService {
     }
 
     @Override
-    public ResultResponse<Boolean> register(User user) {
-        return null;
+    public ResultResponse<Boolean> register(UserNormalDto userNormalDto) {
+
+        //转换成实体类
+        User newUser = userConverter.dtoToEntity(userNormalDto);
+
+        if (!authMapper.insertUser(newUser))
+            return ResultResponseUtils.success(false,"注册失败");
+
+        return ResultResponseUtils.success(true,"注册成功");
     }
 }
