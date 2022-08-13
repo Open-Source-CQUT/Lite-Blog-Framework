e0e120b2f7bf7474d01d000a70083c4222bb5d4a
246859 <2633565580@qq.com>
2022-08-13 16:10:37 +0800

    refactor(web.utils): refactor DateUtils.java
    重构了代码，优化逻辑

--- Lite-Blog-Web/src/main/java/com/liteweb/utils/tool/DateUtils.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/utils/tool/DateUtils.java
@@ -10,6 +10,14 @@ public class DateUtils {
     public static final String DEFAULT_TIME_PATTERN_DETAILS = "HH:mm:ss:SSS";
     public static final String DEFAULT_TIME_PATTERN_ROUGH = "HH:mm:ss";
 
+    public static final Long SECONDS = 1000L;
+
+    public static final Long MINUTES = SECONDS * 60;
+
+    public static final Long HOURS = MINUTES * 60;
+
+    public static final Long DAY = HOURS * 24;
+
     public static String formatNow(){
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(String.format("%s %s",DEFAULT_DATE_PATTERN,DEFAULT_TIME_PATTERN_ROUGH));
