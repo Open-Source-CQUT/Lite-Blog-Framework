127847fdd2f3cd0cfa0176d8f3285c56764e3c15
246859 <2633565580@qq.com>
2022-08-13 16:13:01 +0800

    feature(web): finished regisger

--- Lite-Blog-Web/src/main/java/com/liteweb/service/auth/AuthService.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/service/auth/AuthService.java
@@ -1,15 +1,16 @@
 package com.liteweb.service.auth;
 
+import com.liteweb.dto.auth.UserNormalDto;
 import com.liteweb.dto.global.ResultResponse;
 import com.liteweb.entity.auth.User;
+import com.liteweb.vo.Auth.UserVo;
 
 public interface AuthService {
 
 
     ResultResponse<Boolean> login(String mail,String password);
 
-    ResultResponse<Boolean> register(User user);
-
+    ResultResponse<Boolean> register(UserNormalDto userNormalDto);
 
 
 }
