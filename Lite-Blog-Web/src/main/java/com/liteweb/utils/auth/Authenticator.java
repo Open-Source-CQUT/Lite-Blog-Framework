59350c7dbcae2b22914a3673266b5571ea5e7c4f
246859 <2633565580@qq.com>
2022-08-16 13:28:01 +0800

    feature(web): rewrite method and add method
    重写校验方法，且新增了几个工具方法

--- Lite-Blog-Web/src/main/java/com/liteweb/utils/auth/Authenticator.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/utils/auth/Authenticator.java
@@ -1,11 +1,16 @@
 package com.liteweb.utils.auth;
 
 import com.alibaba.fastjson.JSON;
-import com.liteweb.entity.auth.User;
+import com.liteweb.dto.global.JwtToken;
+import com.liteweb.exception.auth.AuthException;
 import com.liteweb.utils.serializer.RedisCache;
+import com.liteweb.utils.tool.DateUtils;
+import com.liteweb.vo.Auth.UserTokenVo;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.Objects;
 
 @Component
@@ -16,24 +21,126 @@ public class Authenticator {
     RedisCache redisCache;
 
 
-    public Boolean authenticate(String token){
+    /**
+     * 校验access-token
+     *
+     * @param token access-token
+     * @return 是否成功
+     */
+    public Boolean authenticateAccessToken(String token) {
 
-        try {
-            //读取payload
-            String payload = JwtUtil.parseJWT(token).getSubject();
+        //读取payload
+        String payload = JwtUtil.parseAccessJwt(token).getSubject();
 
-            //JSON转换
-            User user = JSON.parseObject(payload,User.class);
+        //读取载荷
+        UserTokenVo userVo = JSON.parseObject(payload, UserTokenVo.class);
 
-            //读取Redis缓存中的用户
-            if (Objects.isNull(redisCache.getCacheObject(user.getMail())))
-                return false;
-
-        }catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
+        //redis中获取不到则过期
+        if (Objects.isNull(
+                redisCache.getCacheObject(
+                        JwtUtil.getRedisAccessKey(
+                                userVo.getMail(),
+                                userVo.getUuid())
+                )))
+            throw new ExpiredJwtException(null, null, null);
 
         return true;
     }
+
+    public Boolean authenticateRefreshToken(String refreshToken, String accessToken) throws AuthException {
+
+        Date absolutelyExpireTime = null;
+
+        UserTokenVo accessPayload = null;
+
+        try {
+            JwtUtil.parseAccessJwt(accessToken);
+        } catch (ExpiredJwtException e) {
+
+            //获取预设的过期时间
+            Long expireTime = e.getClaims().getExpiration().getTime();
+
+            //计算出绝对过期时间
+            absolutelyExpireTime = new Date(JwtUtil.JWT_TRANSITION_TTL + expireTime);
+
+            accessPayload = JSON.parseObject(e.getClaims().getSubject(), UserTokenVo.class);
+
+            //这个时候如果还有什么其他的异常直接返回false
+        } catch (Exception e) {
+            return false;
+        }
+
+        //允许过期时间已过
+        if (Objects.isNull(absolutelyExpireTime) || absolutelyExpireTime.getTime() < System.currentTimeMillis())
+            return false;
+
+
+        //读取payload
+        String payload = JwtUtil.parseAccessJwt(refreshToken).getSubject();
+
+        //读取载荷
+        UserTokenVo refreshPayload = JSON.parseObject(payload, UserTokenVo.class);
+
+        //uuid必须相同
+        if (Objects.isNull(accessPayload) || !accessPayload.getUuid().equals(refreshPayload.getUuid()))
+            return false;
+
+        //access登陆时间只能大于等于refresh,绝对不可能小于refresh
+        if (DateUtils.isBefore(accessPayload.getLoginTime(),refreshPayload.getLoginTime()))
+            return false;
+
+        //redis中获取不到则过期,refresh过期用户必须重新登陆
+        if (Objects.isNull(
+                redisCache.getCacheObject(
+                        JwtUtil.getRedisRefreshKey(
+                                refreshPayload.getMail(), refreshPayload.getUuid()
+                        ))))
+            throw new AuthException();
+
+        return true;
+    }
+
+    public JwtToken processAndGetAccessToken(UserTokenVo userVo) {
+
+        //json转换
+        String userVoJsonStr = JSON.toJSONString(userVo);
+
+        //生成access-token
+        JwtToken accessToken = JwtUtil.createAccessJWT(userVoJsonStr);
+
+        //获取redisKey
+        String accessKey = JwtUtil.getRedisAccessKey(userVo.getMail(), userVo.getUuid());
+
+        //redis处理
+        redisCacheToken(accessKey, accessToken);
+
+        return accessToken;
+    }
+
+    public JwtToken processAndGetRefreshToken(UserTokenVo userVo) {
+
+        //json转换
+        String userVoJsonStr = JSON.toJSONString(userVo);
+
+        //生成access-token
+        JwtToken accessToken = JwtUtil.createRefreshJWT(userVoJsonStr);
+
+        //获取redisKey
+        String accessKey = JwtUtil.getRedisRefreshKey(userVo.getMail(), userVo.getUuid());
+
+        //redis处理
+        redisCacheToken(accessKey, accessToken);
+
+        return accessToken;
+    }
+
+    private void redisCacheToken(String key, JwtToken token) {
+
+        //存入redis
+        redisCache.setCacheObject(key, token);
+
+        //设置过期时间
+        redisCache.expire(key, JwtUtil.getTTL(token));
+    }
+
 }
