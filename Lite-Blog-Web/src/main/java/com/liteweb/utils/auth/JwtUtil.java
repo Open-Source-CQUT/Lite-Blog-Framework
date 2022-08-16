2637a38af5630b3a78c833820ee8e6a4d7463ba5
246859 <2633565580@qq.com>
2022-08-16 13:21:01 +0800

    refactor(web.jwtUtils): refactor code
    The method of generating token is reconstructed,
    Wrap the token into jwttoken,
    Several tools and methods have been added

--- Lite-Blog-Web/src/main/java/com/liteweb/utils/auth/JwtUtil.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/utils/auth/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.liteweb.utils.auth;
 
+import com.liteweb.dto.global.JwtToken;
 import com.liteweb.utils.serializer.PasswordEncoder;
 import com.liteweb.utils.tool.DateUtils;
 import io.jsonwebtoken.Claims;
@@ -19,15 +20,22 @@ import java.util.UUID;
 public class JwtUtil {
 
     //短时效token
-    private static final Long JWT_ACCESS_TTL = DateUtils.HOURS * 2;// 2小时
-
+    public static final Long JWT_ACCESS_TTL = DateUtils.HOURS * 2;// 2小时
     //长时效refresh token
-    private static final Long JWT_REFRESH_TTL = DateUtils.DAY * 2;// 2天
+    public static final Long JWT_REFRESH_TTL = DateUtils.DAY * 7;// 7天
 
-    //设置秘钥明文
+    //允许过渡时间
+    public static final Long JWT_TRANSITION_TTL = DateUtils.MINUTES * 10;//10分钟
+
+    //设置header的key
     public static final String JWT_ACCESS_KEY = PasswordEncoder.enCode("Lite-Blog-Access-Key");
 
     public static final String JWT_REFRESH_KEY = PasswordEncoder.enCode("Lite-Blog-Refresh-Key");
+
+    //私钥
+    private static final String JWT_SECRET_ACCESS_KEY = PasswordEncoder.enCode("VegetableProgrammer-Access-key");
+
+    private static final String JWT_SECRET_REFRESH_KEY = PasswordEncoder.enCode("VegetableProgrammer-Refresh-key");
 
     //签发者
     public static final String ISSUER = PasswordEncoder.enCode("Lite-Blog");
@@ -37,19 +45,26 @@ public class JwtUtil {
     }
 
 
-    public static String createJWT(String subject) {
-        return createJWT(subject, null, JWT_ACCESS_KEY);
+    public static JwtToken createAccessJWT(String subject) {
+        return createJWT(subject, JWT_ACCESS_TTL, JWT_ACCESS_KEY, JWT_SECRET_ACCESS_KEY);
     }
 
-
-    public static String createJWT(String subject, Long ttlMillis, String key) {
-        JwtBuilder builder = getJwtBuilder(subject, ttlMillis, getUUID(), key);
-        return builder.compact();
+    public static JwtToken createAccessJWT(String subject, Long ttl) {
+        return createJWT(subject, ttl, JWT_ACCESS_KEY, JWT_SECRET_ACCESS_KEY);
     }
 
-    public static String createRefreshJWT(String subject) {
-        return createJWT(subject, JWT_REFRESH_TTL, JWT_REFRESH_KEY);
+    public static JwtToken createRefreshJWT(String subject) {
+        return createJWT(subject, JWT_REFRESH_TTL, JWT_REFRESH_KEY, JWT_SECRET_REFRESH_KEY);
     }
+
+    public static JwtToken createRefreshJWT(String subject, Long ttl) {
+        return createJWT(subject, ttl, JWT_REFRESH_KEY, JWT_SECRET_REFRESH_KEY);
+    }
+
+    public static JwtToken createJWT(String subject, Long ttlMillis, String key, String scKey) {
+        return jwtTokenWrap(subject, ttlMillis, getUUID(), key, scKey);
+    }
+
 
     /**
      * 构建jwt
@@ -60,36 +75,41 @@ public class JwtUtil {
      * @param key       私钥
      * @return jwtBuilder
      */
-    private static JwtBuilder getJwtBuilder(String subject, Long ttlMillis, String uuid, String key) {
+    private static JwtToken jwtTokenWrap(String subject, Long ttlMillis, String uuid, String key, String scKey) {
 
         //获取签名算法
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
         //设置私钥
-        SecretKey secretKey = generalKey(key);
+        SecretKey secretKey = generalKey(scKey);
 
         //设置签发时间
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
 
         //设置过期时间
-        long expMillis = nowMillis + (ttlMillis == null ? JwtUtil.JWT_ACCESS_TTL : ttlMillis);
+        long expMillis = nowMillis + ttlMillis;
         Date expDate = new Date(expMillis);
 
-        //构建jwt
-        return Jwts.builder()
+        //生成token字符串
+        String token = Jwts.builder()
                 //唯一的ID
                 .setId(uuid)
-                // 主题  可以是JSON数据
+                //主题  可以是JSON数据
                 .setSubject(subject)
-                // 签发者
+                //签发者
                 .setIssuer(ISSUER)
-                // 签发时间
+                //签发时间
                 .setIssuedAt(now)
                 //使用HS256对称加密算法签名, 第二个参数为秘钥
                 .signWith(signatureAlgorithm, secretKey)
                 //设置过期时间
-                .setExpiration(expDate);
+                .setExpiration(expDate)
+                //打包生成token
+                .compact();
+
+        //包装成JwtToken类返回
+        return new JwtToken(key, token);
     }
 
     /**
@@ -108,11 +128,23 @@ public class JwtUtil {
                 .getBody();
     }
 
-    public static String parseAccessJwt(String jwt) {
-        return parseJWT(jwt, JWT_ACCESS_KEY).getSubject();
+    public static Claims parseAccessJwt(String jwt) {
+        return parseJWT(jwt, JWT_ACCESS_KEY);
     }
 
-    public static String parseRefreshJwt(String jwt) {
-        return parseJWT(jwt, JWT_REFRESH_KEY).getSubject();
+    public static Claims parseRefreshJwt(String jwt) {
+        return parseJWT(jwt, JWT_REFRESH_KEY);
+    }
+
+    public static String getRedisAccessKey(String mail,String uuid) {
+        return String.format("%s-%s-%s",uuid,JWT_ACCESS_KEY, mail);
+    }
+
+    public static String getRedisRefreshKey(String mail,String uuid) {
+        return String.format("%s-%s-%s",uuid,JWT_REFRESH_KEY, mail);
+    }
+
+    public static Long getTTL(JwtToken token) {
+        return JWT_ACCESS_KEY.equals(token.getJwtKey()) ? JWT_ACCESS_TTL : JWT_REFRESH_TTL;
     }
 }
