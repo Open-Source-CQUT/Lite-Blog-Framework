c5ea6ffc2af511e78a4973b803baf16734071930
246859 <2633565580@qq.com>
2022-08-20 12:49:39 +0800

    refactor(web): change some file structs

--- Lite-Blog-Web/src/main/java/com/liteweb/modules/common/controller/error/LiteBlogExceptionController.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/modules/common/controller/error/LiteBlogExceptionController.java
@@ -1,7 +1,7 @@
 package com.liteweb.modules.common.controller.error;
 
 import com.liteweb.modules.common.dto.ResultResponse;
-import com.liteweb.utils.tool.ResultResponseUtils;
+import com.liteweb.modules.common.utils.ResultResponseUtils;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
