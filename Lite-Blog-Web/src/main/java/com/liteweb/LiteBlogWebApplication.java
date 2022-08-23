5665c3176c4ec17c7d13f16b669267e97d0b2786
246859 <2633565580@qq.com>
2022-08-23 18:34:27 +0800

    feature(web): 启动类添加args参数

--- Lite-Blog-Web/src/main/java/com/liteweb/LiteBlogWebApplication.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/LiteBlogWebApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@SpringBootApplication
+
+
 @CrossOrigin
 @MapperScan("com.liteweb.modules.**.dao")
+@SpringBootApplication
 public class LiteBlogWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LiteBlogWebApplication.class);
+        SpringApplication.run(LiteBlogWebApplication.class,args);
     }
 }
