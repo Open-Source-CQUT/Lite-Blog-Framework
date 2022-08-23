37e65d96092679fd83d065cff53391a7c3176052
246859 <2633565580@qq.com>
2022-08-23 18:30:16 +0800

    refactor(mapper): 对数据库表名修改做适配

--- Lite-Blog-Web/src/main/java/com/liteweb/modules/auth/dao/AuthMapper.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/modules/auth/dao/AuthMapper.java
@@ -1,10 +1,11 @@
 package com.liteweb.modules.auth.dao;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.liteweb.modules.auth.entity.User;
 
 import java.util.Optional;
 
-public interface AuthMapper {
+public interface AuthMapper extends BaseMapper<User> {
 
     Optional<User> getUser(String mail);
 
--- Lite-Blog-Web/src/main/java/com/liteweb/modules/cos/dao/CosMapper.java
+++ Lite-Blog-Web/src/main/java/com/liteweb/modules/cos/dao/CosMapper.java
@@ -1,11 +1,12 @@
 package com.liteweb.modules.cos.dao;
 
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.liteweb.modules.cos.entity.File;
 
 import java.util.Optional;
 
-public interface CosMapper {
+public interface CosMapper extends BaseMapper<File> {
 
     Boolean insertFile(File file);
 
