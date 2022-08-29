aba98c946d4c905e8d4fea59b0460524353cfaaa
246859 <2633565580@qq.com>
2022-08-29 16:22:09 +0800

    fix(cors): 从根本上解决了复杂请求的跨域问题

--- Lite-Blog-Auth/src/main/java/com/lite/auth/interceptor/CorsInterceptor.java
+++ Lite-Blog-Auth/src/main/java/com/lite/auth/interceptor/CorsInterceptor.java
@@ -1,5 +1,8 @@
 package com.lite.auth.interceptor;
 
+import com.lite.auth.config.CorsConfig;
+import com.lite.system.utils.SystemStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -17,13 +20,34 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class CorsInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    CorsConfig corsConfig;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        //如果是Options请求,拦截后直接返回200
-        if (HttpMethod.OPTIONS.name().equals(request.getMethod())){
-            response.setStatus(HttpStatus.OK.value());
+        //如果是Options请求,拦截后直接返回203
+        if (HttpMethod.OPTIONS.name().equals(request.getMethod())) {
+
+            // * 这里因为拦截了OPTION请求，返回false的话是不会有响应头的，需要我们自己写返回头
+
+            //设置允许的访问源
+            response.setHeader("Access-Control-Allow-Origin", SystemStringUtils.concatWithComma(corsConfig.getAllowedOrigins()));
+            //设置验证
+            response.setHeader("Access-Control-Allow-Credentials", String.valueOf(corsConfig.getAllowedCredentials()));
+            //设置允许的请求类型
+            response.setHeader("Access-Control-Allow-Methods", SystemStringUtils.concatWithComma(corsConfig.getAllowedMethods()));
+            //设置超时时间
+            response.setHeader("Access-Control-Max-Age", String.valueOf(corsConfig.getMaxAge()));
+            //设置允许的请求头参数
+            response.setHeader("Access-Control-Allow-Headers", SystemStringUtils.concatWithComma(corsConfig.getAllowedHeaders()));
+
+            //返回203
+            response.setStatus(HttpStatus.NON_AUTHORITATIVE_INFORMATION.value());
+
+            //拦截掉，不让继续往下走
             return false;
         }
+
         return true;
     }
 }
