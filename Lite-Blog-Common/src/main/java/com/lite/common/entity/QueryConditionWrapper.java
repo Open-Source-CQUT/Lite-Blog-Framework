7e62ca74b7ba73994e4a070cc0788331115596f6
246859 <2633565580@qq.com>
2022-10-25 22:12:19 +0800

    feat(common): 增加多条件查询实体包装类

---
+++ Lite-Blog-Common/src/main/java/com/lite/common/entity/QueryConditionWrapper.java
@@ -0,0 +1,14 @@
+package com.lite.common.entity;
+
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+
+/**
+ * @author Stranger
+ * @version 1.0
+ * @description: 查询条件包装抽象类，多用于实体类多条件查询
+ * @date 2022/10/25 22:08
+ */
+public abstract class QueryConditionWrapper<T> {
+
+    public abstract LambdaUpdateWrapper<T> buildWrapper();
+}
