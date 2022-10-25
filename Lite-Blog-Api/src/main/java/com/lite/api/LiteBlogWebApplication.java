8063b4dab1592cdd5a2a5a72656a9289457c7b85
246859 <2633565580@qq.com>
2022-10-25 13:53:40 +0800

    fix(api): 修复了mapper扫描路径问题

--- Lite-Blog-Api/src/main/java/com/lite/api/LiteBlogWebApplication.java
+++ Lite-Blog-Api/src/main/java/com/lite/api/LiteBlogWebApplication.java
@@ -3,7 +3,6 @@ package com.lite.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 // * 开启跨域注解
 @CrossOrigin
 // * Mapper注解
-@MapperScan("com.lite.**.dao")
+@MapperScan({"com.lite.**.dao", "com.lite.**.mapper"})
 // * 声明式事务开启注解
 @EnableTransactionManagement
 // * 扫描包的路径，多模块注入必需
@@ -21,6 +20,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @EnableCaching
 public class LiteBlogWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LiteBlogWebApplication.class,args);
+        SpringApplication.run(LiteBlogWebApplication.class, args);
     }
 }
