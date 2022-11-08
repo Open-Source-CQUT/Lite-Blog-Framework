cd8ae6a712fd10dbf73ad0a998ba19937d43d1e3
246859 <2633565580@qq.com>
2022-11-08 17:56:26 +0800

    feat(auth): LiteBlogContextUtils 增加获取客户端IP地址的方法

--- Lite-Blog-Auth/src/main/java/com/lite/auth/utils/LiteBlogContextUtils.java
+++ Lite-Blog-Auth/src/main/java/com/lite/auth/utils/LiteBlogContextUtils.java
@@ -1,8 +1,8 @@
 package com.lite.auth.utils;
 
+import com.lite.auth.vo.UserTokenVo;
 import com.lite.common.serializer.RedisCache;
 import com.lite.common.utils.JwtUtil;
-import com.lite.auth.vo.UserTokenVo;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,6 +10,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
 
 @Getter
 @Component
@@ -37,9 +38,41 @@ public class LiteBlogContextUtils {
         return JwtUtil.parseObject(request.getHeader(JwtUtil.JWT_ACCESS_KEY), UserTokenVo.class, JwtUtil.JWT_ACCESS_KEY);
     }
 
-    public void initLocalUserInfo(UserTokenVo userTokenVo){
+    public void initLocalUserInfo(UserTokenVo userTokenVo) {
         localUserInfo.set(userTokenVo);
     }
+
+    public String getClientIpAddress() {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+            if (ip.equals("127.0.0.1")) {
+                //根据网卡取本机配置的IP
+                InetAddress inet = null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                assert inet != null;
+                ip = inet.getHostAddress();
+            }
+        }
+        // 多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (ip != null && ip.length() > 15) {
+            if (ip.indexOf(",") > 0) {
+                ip = ip.substring(0, ip.indexOf(","));
+            }
+        }
+        return ip;
+    }
+
 
     public UserTokenVo getLocalUserInfo() {
         return localUserInfo.get();
